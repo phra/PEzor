e692d49f6767de925a137e55489963b85e32f094
phra <greensoncio@gmail.com>
2021-07-09 01:57:12 +0200

    release: bump 3.0.1

--- PEzor.sh
+++ PEzor.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION="3.0.0"
+VERSION="3.0.1"
 
 cowsay -f dragon 'PEzor!! v'$VERSION 2>/dev/null || echo 'PEzor!! v'$VERSION
 echo '---------------------------------------------------------------------------'
