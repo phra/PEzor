53d345241c059066fce53180e806c6d37e0f2017
Francesco Soncina <phra@users.noreply.github.com>
2021-10-27 01:28:21 +0200

    release: bump v3.1.0

--- PEzor.sh
+++ PEzor.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION="3.0.5"
+VERSION="3.1.0"
 
 cowsay -f dragon 'PEzor!! v'$VERSION 2>/dev/null || echo 'PEzor!! v'$VERSION
 echo '---------------------------------------------------------------------------'
