5059f9c9fd5ddfadab0da343bc537d300b4e8508
godylockz <81207744+godylockz@users.noreply.github.com>
2022-12-07 14:34:58 -0500

    Fix testing with mimikatz

--- test.sh
+++ test.sh
@@ -2,12 +2,8 @@
 
 INSTALL_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-$INSTALL_DIR/install.sh --llvm &&
-
-export PATH=$PATH:~/go/bin/:$INSTALL_DIR:$INSTALL_DIR/deps/donut/:$INSTALL_DIR/deps/wclang/_prefix_PEzor_/bin/ &&
-
-wget https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20200519/mimikatz_trunk.zip &&
-
+rm -rf mimikatz
+wget https://github.com/gentilkiwi/mimikatz/releases/latest/download/mimikatz_trunk.zip -O mimikatz_trunk.zip &&
 unzip -d mimikatz mimikatz_trunk.zip &&
 
 bash -c "$INSTALL_DIR/PEzor.sh -64 mimikatz/x64/mimikatz.exe -z 2" &&
