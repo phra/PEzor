1c02cf96fb211fc9e1864a97b96a044005fe34dd
phra <greensoncio@gmail.com>
2023-08-18 20:21:18 +0200

    fix: update install script for latest kali

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ INSTALL_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd $INSTALL_DIR &&
 
-apt update &&
-apt install -y python3-pip wget unzip build-essential cmake autotools-dev git clang golang mingw-w64 libcapstone-dev libssl-dev cowsay mono-devel &&
-pip3 install xortool &&
+sudo apt update &&
+sudo apt install -y python3-pip wget unzip build-essential cmake autotools-dev git clang golang mingw-w64 libcapstone-dev libssl-dev cowsay mono-devel &&
+pip3 install --no-warn-script-location xortool &&
 
 mkdir -p deps &&
 cd deps &&
@@ -31,25 +31,26 @@ mkdir -p build &&
 cd build &&
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DLLVM_TARGETS_TO_BUILD="X86" -G "Unix Makefiles" .. &&
 make &&
-make install &&
+sudo make install &&
 cd ../..)
 ) &&
 
-go get github.com/EgeBalci/sgn &&
+go install github.com/EgeBalci/sgn@latest &&
 
 (ls wclang 2>/dev/null 1>&2 || (git clone --depth 1 https://github.com/tpoechtrager/wclang.git &&
 cd wclang &&
 cmake -DCMAKE_INSTALL_PREFIX=_prefix_PEzor_ . &&
 make &&
-make install &&
+sudo make install &&
 cd ..)
 ) &&
 
 cd .. &&
 
-wget https://www.cobaltstrike.com/downloads/beacon.h &&
+wget -O beacon.h https://hstechdocs.helpsystems.com/manuals/cobaltstrike/current/userguide/content/beacon.h &&
 
 (grep -q _prefix_PEzor_ ~/.bashrc || echo "export PATH=\$PATH:~/.local/bin/:~/go/bin/:$INSTALL_DIR:$INSTALL_DIR/deps/donut/:$INSTALL_DIR/deps/wclang/_prefix_PEzor_/bin/") >> ~/.bashrc &&
+(grep -q _prefix_PEzor_ ~/.zshrc || echo "export PATH=\$PATH:~/.local/bin/:~/go/bin/:$INSTALL_DIR:$INSTALL_DIR/deps/donut/:$INSTALL_DIR/deps/wclang/_prefix_PEzor_/bin/") >> ~/.zshrc &&
 
 export PATH=$PATH:~/go/bin/:$INSTALL_DIR:$INSTALL_DIR/deps/donut/:$INSTALL_DIR/deps/wclang/_prefix_PEzor_/bin/ &&
 
