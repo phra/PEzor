e6e30343752978e143d2b9d176d460e256940970
phra <greensoncio@gmail.com>
2023-09-26 15:50:43 +0200

    release: v3.3.0

--- PEzor.sh
+++ PEzor.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION="3.2.1"
+VERSION="3.3.0"
 
 cowsay -f dragon 'PEzor!! v'$VERSION 2>/dev/null || echo 'PEzor!! v'$VERSION
 echo '---------------------------------------------------------------------------'
